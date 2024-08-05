53c754fe0f0e027238507042fe428d01d9f4b133
YIU <usaginya@vip.qq.com>
2024-08-05 20:51:12 +0800

    chore: 输出登录凭证时的错误响应
    把不能解析的页面响应内容输出到日志

--- src/api/skland.ts
+++ src/api/skland.ts
@@ -29,29 +29,48 @@ export async function signIn(grant_code: string) {
 
   return data.data
 }
+
+
 /**
  * 通过登录凭证和森空岛用户的 token 获取角色绑定列表
  * @param cred 鹰角网络通行证账号的登录凭证
  * @param token 森空岛用户的 token
  */
 export async function getBinding(cred: string, token: string) {
-  if (cred == null) return { list: [{ appCode: null, appName: 'getBindding(cred = null)' }] }
-  const url = new URL(BINDING_URL)
-  const [sign, headers] = generateSignature(token, url)
-  const response = await fetch(BINDING_URL, {
-    headers: Object.assign(headers, { sign, cred }),
-  })
-
-  const json = await response.json()
-  const data = json as BindingResponse
-  if (data.code !== 0) {
-    data.data[0].appCode = null
-    data.data[0].appName = data.message
-    console.warn(JSON.stringify(json, null, 2))
+  if (cred == null) {
+    return { list: [{ appCode: null, appName: 'getBinding(cred = null)' }] };
   }
 
-  return data.data
+  const url = new URL(BINDING_URL);
+  const [sign, headers] = generateSignature(token, url);
+  const requestOptions = {
+    headers: { ...headers, sign, cred },
+  }
+
+  let response: Response | null = null;
+
+  try {
+    response = await fetch(BINDING_URL, requestOptions)
+    const json = await response.json()
+    const data = json as BindingResponse
+
+    if (data.code !== 0) {
+      data.data[0].appCode = null;
+      data.data[0].appName = data.message;
+      console.warn(JSON.stringify(json, null, 2));
+    }
+    return data.data;
+
+  } catch (error) {
+    if (response) {
+      console.error('Response body:', await response.text());
+    } else {
+      console.error('Fetch error without response:', error);
+    }
+    throw error;
+  }
 }
+
 
 /**
  * 登岛检票
@@ -70,6 +89,8 @@ export async function checkIn(cred: string, token: string, id: SklandBoard) {
   const data = await response.json() as { code: number, message: string, timestamp: string }
   return data
 }
+
+
 /**
  * 明日方舟每日签到
  * @param cred 鹰角网络通行证账号的登录凭证
